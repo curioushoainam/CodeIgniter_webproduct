3f97808a317978998860e3d6fea34b442c21e0aa
curioushoainam <hoainamtruongute@gmail.com>
2018-10-28 17:27:27 +0700

    cut layout excluded the content section

--- application/views/site/layout.php
+++ application/views/site/layout.php
@@ -4,6 +4,40 @@
 	<?php $this->load->view('site/head'); ?>
 </head>
 <body>
-	my layout
+	<a href="#" id="back_to_top">
+		<img src="<?= public_url() ?>site/images/top.png">
+	</a>
+
+	<div class="wraper">
+		<div class="header">
+			<?php $this->load->view('site/header'); ?>
+		</div>
+
+		<div class="container">
+			<div class="left">
+				<?php $this->load->view('site/left'); ?>
+			</div>
+
+			<div class="content">
+				<?php echo $temp; ?>
+			</div>
+
+			<div class="right">
+				<?php $this->load->view('site/right'); ?>				
+			</div>
+
+			<div class="clear"></div>
+		</div>
+	</div>
+
+	<center>
+		<img src="<?= public_url()?>site/images/bank.png"> 
+	</center>
+
+	<div class="footer">
+		<?php $this->load->view('site/footer'); ?>
+	</div>
+
+	
 </body>
 </html>
